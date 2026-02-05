dfad6e402241bcd0bf4fb2ddf9e44d7feb94fa0d
Shivam Patel <130204492+shivampatel183@users.noreply.github.com>
2026-02-05 23:03:38 +0530

    Make auth pages and navbar mobile responsive

--- src/app/Component/navbar/navbar.component.ts
+++ src/app/Component/navbar/navbar.component.ts
@@ -1,10 +1,22 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  standalone: true,
+  imports: [RouterLink, NgIf],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  isMobileMenuOpen = false;
+
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
+
+  closeMobileMenu() {
+    this.isMobileMenuOpen = false;
+  }
+}
